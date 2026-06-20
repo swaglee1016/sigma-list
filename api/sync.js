e8b2f20be352a8f03992c241a5e8c05edae42301
Swag Lee <3222966568@qq.com>
2026-06-20 13:12:15 +0800

    fix: check:true api route, filesystem SPA fallback

--- api/sync.js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-  const url = new URL(req.url, 'http://localhost');
-  const doc = url.searchParams.get('doc');
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true, doc, env: !!process.env.TIDB_PASSWORD }));
-};
