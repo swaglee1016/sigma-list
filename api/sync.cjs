23d96b1eea2dd60dceaa857efb0973a05e140f2d
Swag Lee <3222966568@qq.com>
2026-06-20 12:34:56 +0800

    fix: ESM mjs with createRequire for mysql2

--- api/sync.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true, env: !!process.env.TIDB_PASSWORD }));
-};
