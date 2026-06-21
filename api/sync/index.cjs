e00ad41e4b231e1581afc7bcc600f9a3ca87afc7
Swag Lee <3222966568@qq.com>
2026-06-21 10:56:09 +0800

    fix: remove api/sync.mjs, vercel.json clean

--- api/sync/index.cjs
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (req, res) => {
-  const url = new URL(req.url, 'http://localhost');
-  const doc = url.searchParams.get('doc');
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ ok: true, doc, env: !!process.env.TIDB_PASSWORD }));
-};
